cb2138dc6eb52a56a055fd295af83936a6d7bcb1
Arne Franken <arne.franken@coremedia.com>
2014-02-17 17:50:44 +0100

    fix package name, move virtualbox to general install, xquartz to home install.

--- brew/brew-cask-install-work.sh
+++ brew/brew-cask-install-work.sh
@@ -9,7 +9,6 @@ brew cask install diffmerge
 brew cask install owncloud
 brew cask install time-tracker-mac
 brew cask install vagrant
-brew cask install virtualbox
 brew cask install vmware-fusion
 #add later
 #brew cask install hipchat
