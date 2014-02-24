62202df885048fc55ddfb9cde697afc38c99ca67
Arne Franken <arne.franken@coremedia.com>
2014-02-24 09:20:36 +0100

    ubuntu bootstrap

---
+++ ubuntu/ubuntu-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install openssh-server
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install build-essential
+sudo apt-get install curl
+
+sudo apt-get install maven
+curl https://raw.github.com/juven/maven-bash-completion/master/bash_completion.bash -o ~/.maven_bash_completion.bash
+sudo apt-get install nodejs
+sudo apt-get install phantomjs
+
