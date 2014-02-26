ea53c574debf643339c61722b3f5a9aba9ad60a1
Arne Franken <arne.franken@coremedia.com>
2014-02-26 22:02:10 +0100

    use fancy shebang

--- bash/bash-bootstrap.sh
+++ bash/bash-bootstrap.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo '. ~/.dotfiles/bash/.profile' > ~/.profile
 source ~/.profile
--- brew/brew-cask-install-home.sh
+++ brew/brew-cask-install-home.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # be sure to install general applications before running installation
 /bin/bash brew-cask-install.sh
