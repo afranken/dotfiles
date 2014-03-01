04abeb45dd46f4c605b82056cea1fd0d9653395e
Arne Franken <coding@techotronic.de>
2014-03-01 18:12:12 +0100

    add config

--- git/git-bootstrap.sh
+++ git/git-bootstrap.sh
@@ -3,8 +3,14 @@
 git config --global user.email arne.franken@coremedia.com
 git config --global user.name "Arne Franken"
 
+#turn all color options on (with git 1.5.5+)
+git config --global color.ui "auto"
+
 #Configure line endings for operation system
 git config --global core.autocrlf input
+
+#enable aut-detection for number of threads to use (good for multi-CPU or multi-core computers) for packing repositories
+git config --global pack.threads "0"
 
 #Configure Git to detect renames
 git config --global diff.renamelimit 0
