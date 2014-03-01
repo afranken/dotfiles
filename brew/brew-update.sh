7ba0661e93cf00328aa8dc2f503dd28ac4e86781
Arne Franken <arne.franken@coremedia.com>
2014-03-01 18:09:42 +0100

    move upgrade to aliases

--- brew/brew-update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# be sure to bootstrap before running script
-/bin/bash "$DOTFILES_PATH/brew/"brew-bootstrap.sh
-
-brew update && brew upgrade && echo "update and upgrade done."
