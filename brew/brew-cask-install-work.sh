1ec009d4e31aedfe97c0b80c8c4a1fecde539a51
Arne Franken <arne.franken@coremedia.com>
2014-03-09 00:33:17 +0100

    always install idea, diffmerge. Install comicbooklover+sync at home.

--- brew/brew-cask-install-work.sh
+++ brew/brew-cask-install-work.sh
@@ -4,12 +4,8 @@
 /bin/bash brew-cask-install.sh
 
 brew cask install sequel-pro
-brew cask install intellij-idea-ultimate
-brew cask install diffmerge
 brew cask install owncloud
 brew cask install time-tracker-mac
 brew cask install vagrant
 brew cask install vmware-fusion
 brew cask install hipchat
-#add later
-#brew cask install hipchat
