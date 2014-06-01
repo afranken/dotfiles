ca3ec02f8bdd5a08018a346775c59eb163dffc8f
Arne Franken <arne.franken@coremedia.com>
2014-06-01 19:43:04 +0200

    use environment variables in osx script

--- osx/osx-bootstrap.sh
+++ osx/osx-bootstrap.sh
@@ -13,10 +13,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "HAM-ITS0526"
-sudo scutil --set HostName "HAM-ITS0526"
-sudo scutil --set LocalHostName "HAM-ITS0526"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "HAM-ITS0526"
+sudo scutil --set ComputerName $COMPUTER_NAME
+sudo scutil --set HostName $COMPUTER_NAME
+sudo scutil --set LocalHostName $COMPUTER_NAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
 
 # Set standby delay to 24 hours (default is 1 hour)
 sudo pmset -a standbydelay 86400
