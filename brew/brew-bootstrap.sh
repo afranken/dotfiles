aff097f31f116abb33c27394abc56175f177c0ba
Arne Franken <arne.franken@coremedia.com>
2015-04-09 17:23:52 +0200

    link Caskroom and Cellar for easy access.

--- brew/brew-bootstrap.sh
+++ brew/brew-bootstrap.sh
@@ -14,3 +14,9 @@ fi
 ## get access to updated versions of utilities provided by OSX
 brew tap homebrew/dupes
 brew tap homebrew/homebrew-versions
+
+##link Cellar into User Home for easy access from various applications (e.g. for setting JDKs in IntelliJ Idea)
+if [ ! -f ~/Cellar ];
+then
+  ln -s /usr/local/Cellar ~/Cellar
+fi
