18e9d0b450b556fad5831de057487786d498c1cc
Arne <arne.franken@coremedia.com>
2015-04-26 21:19:46 +0200

    re-organize installed packages, replace Bash

--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -4,6 +4,10 @@
 /bin/bash brew-bootstrap.sh
 
 FORMULAE=(
+  ### TERMINAL / BASH
+  bash
+  terminal-notifier
+  
   ### MISC
   python
   phantomjs
@@ -14,10 +18,6 @@ FORMULAE=(
   wget
   nodejs
   groovy
-  
-  ### TERMINAL / BASH
-  bash
-  terminal-notifier
   
   ### GIT
   git
@@ -47,3 +47,9 @@ if [ ! -f ~/.maven_bash_completion.bash ];
 then
   curl https://raw.github.com/juven/maven-bash-completion/master/bash_completion.bash -o ~/.maven_bash_completion.bash
 fi
+
+### Set newly installed bash as default (otherwise, shell scripts will use the 3.0 bash if the shebang is "#!/bin/bash")
+# Add the new shell to the list of allowed shells
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# Change to the new shell
+chsh -s /usr/local/bin/bash
