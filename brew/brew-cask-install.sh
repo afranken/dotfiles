420c3a14fa3a2403bfa318dbb998c0671f525979
Arne Franken <coding@techotronic.de>
2015-05-13 14:39:44 +0200

    use unofficial "bash strict mode"

--- brew/brew-cask-install.sh
+++ brew/brew-cask-install.sh
@@ -1,7 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
+###
+# use "bash strict mode", @see http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# https://sipb.mit.edu/doc/safe-shell/
+set -euf
+if [ "${BASH_VERSINFO[0]}" -ge 3 ]; then
+  set -o pipefail
+fi
+IFS=$'\n\t'
+###
+# use command debugging
+#set -x
+
 
 # be sure to bootstrap before running installation
-/bin/bash brew-cask-bootstrap.sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+${DIR}/brew-cask-bootstrap.sh
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 CASKS=(
   alfred
--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -1,7 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+###
+# use "bash strict mode", @see http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# https://sipb.mit.edu/doc/safe-shell/
+set -euf
+if [ "${BASH_VERSINFO[0]}" -ge 3 ]; then
+  set -o pipefail
+fi
+IFS=$'\n\t'
+###
+# use command debugging
+#set -x
 
 # be sure to bootstrap before running installation
-/bin/bash brew-bootstrap.sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+${DIR}/brew-bootstrap.sh
 
 FORMULAE=(
   ### TERMINAL / BASH
