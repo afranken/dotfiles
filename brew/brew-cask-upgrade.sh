5fde11389e6281369440176673ffb34cb653e307
Arne Franken <arne.franken@coremedia.com>
2015-05-18 10:03:51 +0200

    convert script to bash
    this was not really necessary, but I like to have the scripts in this workspace bash-centric keep it simple.

--- brew/brew-cask-upgrade.sh
+++ brew/brew-cask-upgrade.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
-#taken from https://gist.github.com/c00kiemon5ter/3730069b6c920841a3ca
+#!/bin/bash
+###
+# use "bash strict mode", @see http://redsymbol.net/articles/unofficial-bash-strict-mode/ 
+# https://sipb.mit.edu/doc/safe-shell/
+set -eu # set -f will not work since this will disable filename expansion (globbing) upon seeing *, ?, etc.
+if [ "${BASH_VERSINFO[0]}" -ge 3 ]; then
+  set -o pipefail
+fi
+IFS=$'\n\t'
+###
+# use command debugging
+#set -x
+
+#adapted from https://gist.github.com/c00kiemon5ter/3730069b6c920841a3ca
 
 help=0
 latest=0
@@ -7,60 +19,59 @@ verbose=0
 status=0
 
 usage() {
-	cat <<-EOF
-	${0##*/} [options]
+cat <<-EOF
+${0##*/} [options]
 
-	  options:
-	    -h  show help dialog
-	    -l  reinstall packages with version "latest"
-	    -v  verbose output
-	EOF
-	exit "$status"
+  options:
+    -h  show help dialog
+    -l  reinstall packages with version "latest"
+    -v  verbose output
+EOF
+exit "${status}"
 }
 
-for opt
-do case "$opt" in
-	'-h') usage ;;
-	'-l') latest=1 ;;
-	'-v') verbose=1 ;;
-	*) status=1 usage ;;
-esac
+while getopts 'vlh' OPTION ; do
+  case "${OPTION}" in
+    h) usage;;
+    l) latest=1;;
+    v) verbose=1;;
+    *) status=1 && usage;;
+  esac
 done
 
 set -- $(brew cask list)
 sentinel='/'
 
-oldIFS="$IFS"
-IFS="$sentinel"
-apps="$sentinel$*$sentinel"
-IFS="$oldIFS"
+oldIFS="${IFS}"
+IFS="${sentinel}"
+apps="${sentinel}$*${sentinel}"
+IFS="${oldIFS}"
 
-for appdir in /opt/homebrew-cask/Caskroom/*
-do
-	[ -d "$appdir" ] || continue
-	app="${appdir##*/}"
+for appdir in /opt/homebrew-cask/Caskroom/*; do
+  [ -d "${appdir}" ] || continue
+  app="${appdir##*/}"
 
-	verlocal="$(find "$appdir"/* -type d -print -quit)"
-	verlocal="${verlocal##*/}"
-	verlatest="$(brew cask info "$app" | awk -v app="$app" '$1 == app":" { print $2; exit }')"
+  verlocal="$(find "$appdir"/* -type d -print -quit)"
+  verlocal="${verlocal##*/}"
+  verlatest="$(brew cask info "$app" | awk -v app="${app}" '$1 == app":" { print $2; exit }')"
 
-	case "$apps" in
-		*"$sentinel$app$sentinel"*)
-			if [ "$verbose" -ne 0 ]
-			then
-				printf -- ':: found app: %s\n' "$app"
-				printf -- 'local  version: %s\n' "$verlocal"
-				printf -- 'latest version: %s\n' "$verlatest"
-			fi
-			if [ "$latest" -ne 0 ] && [ "$verlocal" = 'latest' ] || [ "$verlocal" != "$verlatest" ]
-			then brew cask install --force "$app" && [ "$verlocal" != "$verlatest" ] && rm -rf "$appdir/$verlocal"
-			fi
-			;;
-		*)
-			printf -- 'app not found: %s\n' "$app"
-			status=1
-			;;
-	esac
+  case "$apps" in
+    *"${sentinel}${app}${sentinel}"*)
+      if [ "${verbose}" -ne 0 ]
+      then
+        printf -- ':: found app: %s\n' "${app}"
+        printf -- 'local  version: %s\n' "${verlocal}"
+        printf -- 'latest version: %s\n' "${verlatest}"
+      fi
+      if [ "${latest}" -ne 0 ] && [ "${verlocal}" = 'latest' ] || [ "${verlocal}" != "${verlatest}" ]
+        then brew cask install --force "${app}" && [ "${verlocal}" != "${verlatest}" ] && rm -rf "${appdir}/${verlocal}"
+      fi
+      ;;
+    *)
+      printf -- 'app not found: %s\n' "${app}"
+      status=1
+      ;;
+  esac
 done
 
-exit "$status"
+exit "${status}"
