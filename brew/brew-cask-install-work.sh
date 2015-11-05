4ce554ba73229f590d57f0af130dda8c3f1e0a71
Arne Franken <coding@techotronic.de>
2015-11-05 16:14:27 +0100

    install dockertoolbox, cleanup unused software

--- brew/brew-cask-install-work.sh
+++ brew/brew-cask-install-work.sh
@@ -22,11 +22,9 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 CASKS=(
-  owncloud
-  vagrant
-  vmware-fusion
   chromium
   chefdk
+  dockertoolbox
 )
 
 ### INSTALL
