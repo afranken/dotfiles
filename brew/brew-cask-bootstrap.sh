b7f96c0e7fd875cee31a0d6a49cc55e152eaba5f
Arne Franken <coding@techotronic.de>
2016-03-22 10:37:12 +0100

    update the way homebrew cask is installed
    see: https://github.com/caskroom/homebrew-cask/releases/tag/v0.60.0

--- brew/brew-cask-bootstrap.sh
+++ brew/brew-cask-bootstrap.sh
@@ -21,8 +21,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 brew tap caskroom/versions
+brew install homebrew/completions/brew-cask-completion
 
 ##link Caskroom into User Home for easy access from various applications (e.g. for setting JDKs in IntelliJ Idea)
 if [ ! -d ~/Caskroom ];
