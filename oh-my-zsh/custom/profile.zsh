b73c82b2d614432ebf444b5707d010c7aec47ac5
Arne Franken <coding@techotronic.de>
2019-03-05 11:05:57 +0100

    export dummy values to make AWS-Java-SDK happy...
    this is the easiest way - otherwise, we'd have to change the
    provider chain in every project we want to build locally.

--- oh-my-zsh/custom/profile.zsh
+++ oh-my-zsh/custom/profile.zsh
@@ -2,3 +2,7 @@ export DOTFILES_PATH=~/.dotfiles
 export SHELLS_SRC_PATH=${DOTFILES_PATH}/all_shells
 
 source ${SHELLS_SRC_PATH}/.profile
+
+#just dummy values to make the AWS-SDK happy during local tests.
+export AWS_ACCESS_KEY_ID=NONE
+export AWS_SECRET_KEY=NONE
