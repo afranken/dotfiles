d7d376e5637f630a2bcc74fc23f805631be150d3
Arne Franken <coding@techotronic.de>
2021-11-15 22:15:13 +0100

    Let update-lists export to suffixed files like "home"/"work"

--- software/update-lists.sh
+++ software/update-lists.sh
@@ -11,13 +11,19 @@ IFS=$'\n\t'
 # use command debugging
 #set -x
 
+if [[ -z ${1-} ]]; then
+  system_suffix=""
+else
+  system_suffix="_$1"
+fi
+
 base_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 #Mac AppStore - list all installed apps
-mas list >"${base_dir}"/mas-list
+mas list >"${base_dir}"/mas-list"${system_suffix}"
 
 #Brew - list all DIRECTLY installed apps (brew leaves does not list automatically installed dependencies)
-brew leaves >"${base_dir}"/brew-list
+brew leaves >"${base_dir}"/brew-list"${system_suffix}"
 
 #Brew Cask - list all installed apps
-brew list --cask >"${base_dir}"/brew-cask-list
+brew list --cask >"${base_dir}"/brew-cask-list"${system_suffix}"
