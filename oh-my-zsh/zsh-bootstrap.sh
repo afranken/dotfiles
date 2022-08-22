389c85815fe9acaa61c3c68310cd249416803ea3
Arne Franken <coding@techotronic.de>
2022-08-22 16:32:42 +0200

    Use current oh-my-zsh setup and theme

--- oh-my-zsh/zsh-bootstrap.sh
+++ oh-my-zsh/zsh-bootstrap.sh
@@ -12,4 +12,4 @@ IFS=$'\n\t'
 #set -x
 
 #install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
